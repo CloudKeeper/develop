bdaaf40ff5a3b090045c76b01fe71ebcfc80c73b
Cloud_Keeper <Cloud_keeper51@hotmail.com>
2019-01-07 20:55:12 +1100

    Working on basic radio station

--- features/radiostation.py
+++ features/radiostation.py
@@ -1,5 +1,5 @@
 """
-Housing - INOPERABLE
+Radio Station - INOPERABLE
 
 -Character Mixin
 -Exit Obj
@@ -68,34 +68,7 @@ class RadioStation(DefaultScript):
         self.db.channel.msg(self.db.data[self.db.playlist[0]][pos],
                             header=None, senders=None)
         self.db.position += 1
- 
-# encoding=utf-8
-"""
-Songs for radio channels.
-"""
- 
-static = {
- 
-    "Static" : [
-        "The crackle of background static bellows from the speakers",
-    ],
-}
- 
-pokemusic = {
- 
-    "Pokemon Theme": [
-        "♫ I want to be the very best, like no one ever was. ♫",
-        "♫ To catch them is my real test, to train them is my cause. ♫",
-       
-    ],
- 
-    "PokeRap" : [
-        "♫ I want to be the best there ever was ♫",
-        "♫ To beat all the rest, yeah, that's my cause! ♫",
-    ],
-}
- 
- 
+
 # -*- coding: utf-8 -*-
  
 #
@@ -127,3 +100,87 @@ CHANNELHANDLER.update()
 station.msg("Connected", header=None, senders=None)
 create_script("world.script_radio.RadioStation", obj=None)
 """
+
+"""
+Radio Station - INOPERABLE
+
+This is a system for having in-game radio stations which players can tune into
+via radio objects to 'hear' (read) their favourite songs or 'listen to' (read) 
+their favourite radio host.
+
+A 'radio station' is a channel, like that created by by the channelcreate
+command, that players cannot connect to directly. Instead special radio 
+objects are 'tuned' to the different channels and will then announce the 
+messages from that channel into the their current location. 
+
+The messages of the Radio Stations is broadcast by a script that, in this 
+implementation, simply selects a global level string variable at random from a 
+module which contains song lyrics to be split by line and 'broadcast' one at 
+a time in sequence.
+
+Lyric Module -> Radio Station -> Channel -> Radio Object -> Location
+
+# -----------------------------------------------------------------------------
+NOTES:
+- Get list of variables in module and pop next song from list. That will 
+prevent repeats until set finished.
+# -----------------------------------------------------------------------------
+"""
+from evennia.comms.models import ChannelDB
+from evennia.utils import utils
+from evennia import DefaultScript, TICKER_HANDLER
+import random, sys
+
+
+# Default radio messages.
+static = """\
+The crackle of background static bellows from the speakers\
+"""
+
+ball_game = """\
+♫ Take me out to the ball game ♫
+♫ Take me out with the crowd ♫
+♫ Buy me some peanuts and Cracker Jack ♫
+♫ I don't care if I never get back ♫
+♫ Let me root, root, root for the home team ♫
+♫ If they don't win, it's a shame ♫
+♫ For it's one, two, three strikes, you're out ♫
+♫ At the old ball game ♫
+"""
+
+evennia_ad = """\
+We interrupt this program for a word from our sponsors:
+Good morning boys and girls!
+Did you know that Evennia is an open-source library and toolkit...
+made for building multi-player online text games!
+You can easily design your entire game using normal Python modules.
+Get your parents to get you Evennia today!
+Now back to your regular program.\
+"""
+
+
+class RadioStation(DefaultScript):
+    """
+    self.db.source [Python path, absolute path or a module]
+    """
+
+    def at_script_creation(self):
+        self.key = "Radio Station"
+        self.desc = "Script that sends messages to a channel"
+        self.interval = 10  # Repeats after 10 seconds.
+
+        self.db.source = sys.modules[__name__] # Default source file (this one)
+        self.db.channel =  None # Channel to broadcast to.
+        self.db.current = None # Current song.
+
+    def at_repeat(self):
+        """
+
+        """
+
+        # If no current, pick random song from source and break into lines.
+        if not self.db.current:
+            self.db.current = utils.random_string_from_module(self.db.source).splitlines()
+
+        # Broadcast next line.
+        self.db.channel.msg(self.db.current.pop(0))
