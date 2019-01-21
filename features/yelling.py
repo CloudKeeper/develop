082b2c64b29a118d138772e9940cf0c914cb8743
Cloud_Keeper <Cloud_keeper51@hotmail.com>
2019-01-21 21:13:23 +1100

    Extra features

---
+++ features/yelling.py
@@ -0,0 +1,8 @@
+"""
+Loud Sounds - INOPERABLE
+
+Say Command that sends messages to adjoining rooms
+Add function to object that messages contents and the contents of destinations
+set on it's contents.
+
+"""
