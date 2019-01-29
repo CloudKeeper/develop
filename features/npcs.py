31e651540d97fd7ae88cc263414e8ec2e431c1a1
Cloud_Keeper <Cloud_keeper51@hotmail.com>
2019-01-29 21:53:24 +1100

    Work on NPCs

--- features/npcs.py
+++ features/npcs.py
@@ -5,3 +5,46 @@ NPCS - INOPERABLE
 -NPCs by area.
 
 """
+
+from typeclasses.characters import Character
+from evennia.utils.evmenu import EvMenu
+
+
+class Trainer(Character):
+    """
+    Typeclass for trainer characters.
+
+    """
+
+    def at_object_creation(self):
+        """
+        Called only once, when object is first created
+        """
+        super().at_object_creation()
+        self.db.conversation =
+
+    def at_talk(self, caller):
+        """
+        Called when obj targeted by a talk command.
+        """
+
+        # Response if parent has player or NPC has no conversation set.
+        if self.has_player:
+            caller.execute_cmd("Say Hi %s" % self.key)
+            return
+
+        if not self.db.conversation:
+            caller.msg("%s appears too busy to talk right now." % self.key)
+            return
+
+        # Telegraph action to room.
+        caller.location.msg_contents(caller.key + "speaks to " + self.key,
+                                     exclude=caller)
+
+        # Retrieve conversation stored on the object
+        conversation = {}
+        exec self.db.conversation
+
+        EvMenu(caller, conversation, cmdset_mergetype="Replace",
+               startnode="node1", cmdset_priority=1,
+               cmd_on_exit="", npc=self)
