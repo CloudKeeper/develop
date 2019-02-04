09432f6b6d6a45f6710c2451b27020a2d60178e8
Cloud_Keeper <Cloud_keeper51@hotmail.com>
2019-02-04 12:28:18 +1100

    New layout of Remote Characters

--- features/remotecharacter.py
+++ features/remotecharacter.py
@@ -3,8 +3,6 @@ IRC2Puppet - INOPERABLE
 
 Cloud_Keeper 2018
 
--Toggle stashing character after puppetting
-
 The main character will have the session and account attached to it
 Your puppets are a custom typeclass
 You store a reference to your main account on the puppet.db.puppeteer
@@ -15,17 +13,6 @@ your puppets are in the same room, your main character will recieve the
 first message and send it to your client. THen it'll get the second message
 and see it's already gotten it and throw it away etc
 You've already got your commands working so that's fine
-
-Account method.
-
-Account:
-Secondary puppet
-msg = if from secondary puppet (self.db.puppetter = self) check against history
-and boot.
-add puppet.db.puppetkey <>
-add to history
-
-
 
 Command:
 Puppet <Object>
@@ -44,12 +31,15 @@ command
 Createremote
 creates character
 
+@remote > RemoteCharacter > RemoteAccount
 
 # -----------------------------------------------------------------------------
 NOTES:
+-Add tag to msg being sent to account when acting as a puppet.
 -Switch to turn off going to None
 -Stops spam
 -Have custom header, border or something to show it's from  a remote.
+-Toggle stashing character after puppetting
 # -----------------------------------------------------------------------------
 
 In settings add max characters greater than 1:
@@ -63,30 +53,7 @@ from evennia.accounts.bots import Bot
 
 ##############################################################################
 #
-# Remote Typeclasses
-#
-##############################################################################
-
-
-class RemoteCharacter(FILL IN WITH ALL CLASSES):
-    """
-    Collects Mixins to a single reference typeclass
-    """
-    
-    pass
-
-    
-class RemoteAccount(FILL IN WITH ALL CLASSES)):
-    """
-
-    """
-
-    pass
-   
-
-##############################################################################
-#
-# Allow Remote Control of Characters 
+# @Remote Command
 #
 ##############################################################################
 
@@ -101,18 +68,15 @@ class RemoteCmdSet(CmdSet):
 
 class CmdRemote(COMMAND_DEFAULT_CLASS):
     """
-
+    Creation and Deleting completed by @charcreate, @chardelete.
+    Puppeting primary Character completed with @ic
     """
     key = "@remote"
     locks = "cmd:all()"
     help_category = "General"
 
     def func(self):
-        """
-        Creation and Deleting completed by @charcreate, @chardelete.
-        Puppeting primary Character completed with @ic
-        """
-
+        """ """
         # If no args: list available puppets
         if not self.args:
             self.msg(self.account.at_look(target=self.playable,
@@ -131,11 +95,16 @@ class CmdRemote(COMMAND_DEFAULT_CLASS):
 
         # Remote Character
 
+##############################################################################
+#
+# RemoteCharacter Typeclass
+#
+##############################################################################
 
 
-class CharRemoteControl(DefaultCharacter):
+class RemoteCharacter(FILL IN WITH ALL CLASSES):
     """
-
+    Collects Mixins to a single reference typeclass
     """
 
     def at_object_creation(self):
@@ -143,32 +112,45 @@ class CharRemoteControl(DefaultCharacter):
         At creation we hide the 'listener' from view.
         """
         self.db.remote_account = None
-        
+        self.db.no_session_stashing = True
+
     def remote_control(self):
         """
         """
         # command checks lock checking
-        Set remote about attribute
-        create command
-        give caller command set
-        
+        # Set remote attribute
+        # create command
+        # give caller commandset
 
-##############################################################################
-#
-# Toggle hiding Characters after puppetting and remoting
-#
-##############################################################################
-
-class CharToggleStashing(DefaultCharacter):
-    """
-    By Default characters get sent to None when they are un
-    """
-
-    def at_object_creation(self):
+    def msg(self, text=None, from_obj=None, session=None, options=None, **kwargs):
         """
-        At creation we hide the 'listener' from view.
+        If currently a remote puppet, send message to account
         """
-        self.db.no_session_stashing = True
+        if self.db.remote_account:
+            self.db.remote_account.msg(text, from_obj, session, options,
+                                       **kwargs)
+            return
+
+        super(RemoteCharacter, self).msg(text, from_obj, session, options,
+                                         **kwargs)
+
+    def at_post_unremotepuppet(self, account, session=None, **kwargs):
+        """
+        The RemoteCharacter provides a switch to toggle stowing the character
+        away when the account goes ooc/logs off, to allow others to remote
+        puppet the character whilst the account holder is gone.
+
+        Args:
+            account (Account): The account object that just disconnected
+                from this object.
+            session (Session): Session controlling the connection that
+                just disconnected.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+        """
+        if not self.db.remote_account():
+            super(RemoteCharacter, self).at_post_unpuppet(account, session,
+                                                          **kwargs)
 
     def at_post_unpuppet(self, account, session=None, **kwargs):
         """
@@ -189,59 +171,23 @@ class CharToggleStashing(DefaultCharacter):
             if self.location:
                 def message(obj, from_obj):
                     obj.msg("%s has left the game." % self.get_display_name(obj),
-                                                      from_obj=from_obj)
-                self.location.for_contents(message, exclude=[self], 
+                            from_obj=from_obj)
+
+                self.location.for_contents(message, exclude=[self],
                                            from_obj=self)
                 self.db.prelogout_location = self.location
                 self.location = None
 
-    def at_post_unremotepuppet(self, account, session=None, **kwargs):
-        """
-        The RemoteCharacter provides a switch to toggle stowing the character
-        away when the account goes ooc/logs off, to allow others to remote
-        puppet the character whilst the account holder is gone.
-
-        Args:
-            account (Account): The account object that just disconnected
-                from this object.
-            session (Session): Session controlling the connection that
-                just disconnected.
-            **kwargs (dict): Arbitrary, optional arguments for users
-                overriding the call (unused by default).
-        """
-        if not self.db.remote_account():
-            super(RemoteCharacter, self).at_post_unpuppet(account, session,
-                                                          **kwargs)
-
 ##############################################################################
 #
-# Messages to account and Spam protection
+# RemoteAccount Typeclasses
 #
 ##############################################################################
-                
-class CharMsgRouting(DefaultCharacter):
+
+class RemoteAccount(FILL IN WITH ALL CLASSES)):
     """
 
     """
-
-    def msg(self, text=None, from_obj=None, session=None, options=None, **kwargs):
-        """
-        If currently a remote puppet, send message to account
-        """
-        if self.db.remote_account:
-            self.db.remote_account.msg(text, from_obj, session, options,
-                                       **kwargs)
-            return
-
-        super(RemoteCharacter, self).msg(text, from_obj, session, options,
-                                         **kwargs)
-
-                                         
-class AccMsgRouting(DefaultAccount):
-    """
-
-    """
-
     def at_account_creation(self):
         """
         This is called once, the very first time the account is created.
